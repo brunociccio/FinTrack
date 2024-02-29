c01f693c983edd42fc17c55ea902a14b138888e5
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-02-29 09:22:13 -0300

    nova dependencia adicionada / adicionando controller e requestmapping na nossa pagina main.java

--- src/main/java/br/com/fiap/fintrack/FintrackApplication.java
+++ src/main/java/br/com/fiap/fintrack/FintrackApplication.java
@@ -2,12 +2,21 @@ package br.com.fiap.fintrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
+
 public class FintrackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FintrackApplication.class, args);
+	}
+
+	@RequestMapping
+	public String home() {
+		return "FinTrack";
 	}
 
 }
