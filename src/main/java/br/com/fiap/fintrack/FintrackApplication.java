6444e629087e277e9424001da6784f059ce72595
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-02-29 09:44:42 -0300

    criação de uma pasta de controllers com um arquivo java para esses controllers

--- src/main/java/br/com/fiap/fintrack/FintrackApplication.java
+++ src/main/java/br/com/fiap/fintrack/FintrackApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -15,6 +16,7 @@ public class FintrackApplication {
 	}
 
 	@RequestMapping
+	@ResponseBody 
 	public String home() {
 		return "FinTrack";
 	}
