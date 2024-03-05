f8b4c11c3aa23e332ea37596359babd9c7029943
Bruno Ciccio <dev.bruno.ciccio@gmail.com>
2024-03-05 12:04:46 -0300

    criação da classe.categoria e de seus status code

--- src/main/java/br/com/fiap/fintrack/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/fintrack/controller/CategoriaController.java
@@ -1,30 +1,54 @@
 package br.com.fiap.fintrack.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.fintrack.model.Categoria;
 
 
-@Controller
+
+@RestController
+@RequestMapping("categoria")
 public class CategoriaController {
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    List<Categoria> repository = new ArrayList<>();
     
-    @RequestMapping(
-        method = RequestMethod.GET, 
-        path = "/categoria", 
-        produces = "application/json"
-        )    
-    @ResponseBody        
-    public String index() {
-        return """
-                [
-                    {
-                        "id": 1,
-                        "nome": "Alimentação",
-                        "icone": "fast-food"
-                    }
-                ]
-                """;
+    @GetMapping()    
+    public List<Categoria> index() {
+        return repository;
+    }
+
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Categoria create(@RequestBody Categoria categoria) {
+        log.info("cadastrando categoria {}", categoria);
+        repository.add(categoria);
+        return categoria;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> show( @PathVariable Long id){
+        log.info("buscando categoria com id {}", id);
+
+        for(Categoria categoria : repository) {
+            if(categoria.id().equals(id))
+                return ResponseEntity.status(200).body(categoria);
+        }
+        return ResponseEntity.status(404).build();
+
     }
 
 }
