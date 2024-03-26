ede9c85c07bee9ee1866301d5a316110ca5c7a89
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-03-26 11:28:29 -0300

    adicionando nova dependencia / refatorando os files relacionados a movimentacao / adicionando uma nova condicao a properties

--- src/main/java/br/com/fiap/fintrack/model/Movimentacao.java
+++ src/main/java/br/com/fiap/fintrack/model/Movimentacao.java
@@ -7,6 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -14,8 +18,16 @@ import lombok.Data;
 public class Movimentacao {
     @Id @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String descricao;
+
+    @Positive
     private BigDecimal valor;
+
+    @PastOrPresent
     private LocalDate data;
+
     private String tipo; // receita / despesa
 }
